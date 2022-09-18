98225954def516e2d41fae268c7da46f0cefae77
AndreyBulganin <bulganinworkbox@gmail.com>
2022-09-18 13:55:57 +0300

    Json's been added

--- StoreApp/StoreApp/Controllers/AppSearchController.swift
+++ StoreApp/StoreApp/Controllers/AppSearchController.swift
@@ -14,7 +14,29 @@ class AppSearchController: UICollectionViewController, UICollectionViewDelegateF
         
         collectionView.backgroundColor = .white
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
-        
+        fetchItunesApps()
+    }
+    
+    fileprivate func fetchItunesApps() {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return}
+        URLSession.shared.dataTask(with: url) { data, resp, err in
+            if let err = err {
+                print("Failed to fetch apps:", err)
+            }
+//            print(data)
+//            print(String(data: data!, encoding: .utf8))
+            guard let data = data else { return }
+            
+            do {
+                let searchResult = try
+                JSONDecoder().decode(SearchResult.self, from: data)
+                searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+            } catch let jsonErr {
+                print("Failed to decode json:", jsonErr)
+            }
+            
+        }.resume()
     }
     
     func collectionView(_ collectionView: UICollectionView,
@@ -25,7 +47,8 @@ class AppSearchController: UICollectionViewController, UICollectionViewDelegateF
 
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        cell.nameLabel.text = "HERE IS MY APP NAME"
         return cell
     }
     
---
+++ StoreApp/StoreApp/Model/SearchResult.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResult.swift
+//  StoreApp
+//
+//  Created by Andrey Bulganin on 18.09.2022.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
