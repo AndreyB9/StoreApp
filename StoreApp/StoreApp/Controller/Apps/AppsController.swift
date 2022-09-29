9da454fd74374bb34a0638e6bcd7a882490c6fae
AndreyBulganin <bulganinworkbox@gmail.com>
2022-09-30 00:49:06 +0300

    Modification App Row Cells

--- StoreApp/StoreApp/Controller/Apps/AppsController.swift
+++ StoreApp/StoreApp/Controller/Apps/AppsController.swift
@@ -11,7 +11,7 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout  {
     let cellId = "id"
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .yellow
+        collectionView.backgroundColor = .white
         
         collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
     }
@@ -26,6 +26,9 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout  {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 250)
+        return CGSize(width: view.frame.width, height: 300)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 0, bottom: 0, right: 0)
     }
 }
--- StoreApp/StoreApp/Controller/Apps/AppsHorizontalController.swift
+++ StoreApp/StoreApp/Controller/Apps/AppsHorizontalController.swift
@@ -11,9 +11,8 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
 let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .blue
-        
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
+        collectionView.register(AppRowCell.self, forCellWithReuseIdentifier: cellId)
         
         if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .horizontal
@@ -26,7 +25,6 @@ let cellId = "cellId"
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
         return cell
     }
 
@@ -34,7 +32,7 @@ let cellId = "cellId"
     let lineSpacing: CGFloat = 10
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let height = (view.frame.height - 2 * topBottomPadding - 2 * lineSpacing) / 3
-        return .init(width: view.frame.width, height: height)
+        return .init(width: view.frame.width - 30, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -42,6 +40,6 @@ let cellId = "cellId"
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return .init(top: topBottomPadding  , left: 16, bottom: topBottomPadding, right: 16)
+        return .init(top: topBottomPadding  , left: 10, bottom: topBottomPadding, right: 16)
     }
 }
