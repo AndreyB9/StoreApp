78e0a9e506927b0907d535ffc83c75314d7318e0
AndreyBulganin <bulganinworkbox@gmail.com>
2022-10-22 19:01:54 +0300

    Preview Screenshots Cell

---
+++ StoreApp/StoreApp/Views/Apps/Details/PreviewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PreviewCell.swift
+//  StoreApp
+//
+//  Created by Andrey Bulganin on 22.10.2022.
+//
+
+import UIKit
+
+class PreviewCell: UICollectionViewCell {
+ 
+    let previewLabel = UILabel(text: "Preview", font: .boldSystemFont(ofSize: 20), NumberOfLines: 0)
+    let horizontalController = PreviewScreenshotsController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(previewLabel)
+        addSubview(horizontalController.view)
+        
+        horizontalController.view.anchor(top: previewLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 20, left: 0, bottom: 0, right: 0))
+        previewLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: 20, bottom: 0, right: 20))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
