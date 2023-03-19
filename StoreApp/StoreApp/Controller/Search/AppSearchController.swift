d3d9e2d6663e96bc65ac79cce51945696c999603
AndreyBulganin <bulganinworkbox@gmail.com>
2023-03-19 14:46:03 +0300

    Some refactoring, dependency injection

--- StoreApp/StoreApp/Controller/Search/AppSearchController.swift
+++ StoreApp/StoreApp/Controller/Search/AppSearchController.swift
@@ -18,6 +18,12 @@ class AppSearchController: BaseListController, UICollectionViewDelegateFlowLayou
         label.font = UIFont.boldSystemFont(ofSize: 20)
         return label
     }()
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let appId = String(appResults[indexPath.item].trackId)
+        let appDetailController = AppDetailController(appId: appId)
+        navigationController?.pushViewController(appDetailController, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,6 +47,10 @@ class AppSearchController: BaseListController, UICollectionViewDelegateFlowLayou
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
             Service.shared.fetchApps(searchTerm: searchText) { res, err in
+                if let err = err {
+                    print("Failed to fetch apps:", err)
+                    return
+                }
                 self.appResults = res?.results ?? []
                 DispatchQueue.main.async {
                     self.collectionView.reloadData()
